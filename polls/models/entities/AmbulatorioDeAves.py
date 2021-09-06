716aed280488c05453ebf7cf44599912cd5fcbef
Vitor Furlan de Oliveira <vitorfurlan1@hotmail.com>
2021-09-06 09:55:16 -0300

    Projeto criado

---
+++ polls/models/entities/AmbulatorioDeAves.py
@@ -0,0 +1,13 @@
+from _typeshed import Self
+from django.db import models
+
+class AmbulatorioDeAves:
+    def __init__(anamnese, alimentacao, exameFisico, examesComplementares, diagnostico, prognostico, tratamento, retorno):
+        Self.anamnese=anamnese
+        Self.alimentacao=alimentacao
+        Self.exameFisico=exameFisico
+        Self.examesComplementares=examesComplementares
+        Self.diagnostico=diagnostico
+        Self.prognostico=prognostico
+        Self.tratamento=tratamento
+        Self.retorno=retorno
